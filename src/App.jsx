ac4384494eca833cbc3a940074b03e38d02b3a57
Enock <bernardo.enock@vileve.com.br>
2024-11-18 16:56:12 -0300

    feat: Start dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './styles/globalStyles.css'
 
-import MyFirstJSX from "./studies/Tasks/01_MyFirstJSX/MyFirstJSX"
-import MyFirstComponents from "./studies/Tasks/02_MyFirstComponents"
-import PeopleList from "./studies/Tasks/03_PeopleCard/PeopleList"
-import DefaultTemplate from "./studies/Tasks/04_CreatingTemplateComponent/DefaultTemplate"
-import FruitsList from "./studies/Tasks/05_RenderingListFruits/FruitsList"
-import TeacherCardModulesCSS from "./studies/Tasks/06_ModulesCSS"
+import Portifolio from './studies/Projects/Portifolio'
+// import MyFirstJSX from "./studies/Tasks/01_MyFirstJSX/MyFirstJSX"
+// import MyFirstComponents from "./studies/Tasks/02_MyFirstComponents"
+// import PeopleList from "./studies/Tasks/03_PeopleCard/PeopleList"
+// import DefaultTemplate from "./studies/Tasks/04_CreatingTemplateComponent/DefaultTemplate"
+// import FruitsList from "./studies/Tasks/05_RenderingListFruits/FruitsList"
+// import TeacherCardModulesCSS from "./studies/Tasks/06_ModulesCSS"
 
 function App() {
 
   return (
     <>
-     <h1>React Tasks</h1>
-      <DefaultTemplate>
+     {/* <h1>React Tasks</h1> */}
+      {/* <DefaultTemplate>
         <TeacherCardModulesCSS />
         <FruitsList />
         <MyFirstComponents>
           <MyFirstJSX />
           <PeopleList />
         </MyFirstComponents>
-      </DefaultTemplate>
+      </DefaultTemplate> */}
+
+      <Portifolio />
     </>
   )
 }
