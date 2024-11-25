02d904f227014e3de1ff961a7753fcf257c0d6c3
Enock <bernardo.enock@vileve.com.br>
2024-11-25 14:32:35 -0300

    feat: Add more class to Text

--- src/studies/Projects/Portifolio/components/atoms/TextPortfolio.jsx
+++ src/studies/Projects/Portifolio/components/atoms/TextPortfolio.jsx
@@ -2,22 +2,23 @@ import style from './style.module.css'
 
 import PropTypes from 'prop-types'
 
-export default function TextPortfolio({children, styleToken}){
+export default function TextPortfolio({children, styleToken, moreClassStyled}){
 
-  if (styleToken == 'title1') return <h1 className={style.txt_title1}>{children}</h1>
-  if (styleToken == 'title2') return <h2 className={style.txt_title2}>{children}</h2>
-  if (styleToken == 'title3') return <h2 className={style.txt_title3}>{children}</h2>
-  if (styleToken == 'title4') return <h2 className={style.txt_title4}>{children}</h2>
+  if (styleToken == 'title1') return <h1 className={`${style.txt_title1} ${moreClassStyled}`}>{children}</h1>
+  if (styleToken == 'title2') return <h2 className={`${style.txt_title2} ${moreClassStyled}`}>{children}</h2>
+  if (styleToken == 'title3') return <h2 className={`${style.txt_title3} ${moreClassStyled}`}>{children}</h2>
+  if (styleToken == 'title4') return <h2 className={`${style.txt_title4} ${moreClassStyled}`}>{children}</h2>
 
-  if (styleToken == 'headline') return <p className={style.txt_headline}>{children}</p>
-  if (styleToken == 'body') return <p className={style.txt_body}>{children}</p>
-  if (styleToken == 'caption') return <p className={style.txt_caption}>{children}</p>
-  if (styleToken == 'caption_bold') return <p className={style.txt_caption_bold}>{children}</p>
-  if (styleToken == 'detail') return <p className={style.txt_detail}>{children}</p>
+  if (styleToken == 'headline') return <p className={`${style.txt_headline} ${moreClassStyled}`}>{children}</p>
+  if (styleToken == 'body') return <p className={`${style.txt_body} ${moreClassStyled}`}>{children}</p>
+  if (styleToken == 'caption') return <p className={`${style.txt_caption} ${moreClassStyled}`}>{children}</p>
+  if (styleToken == 'caption_bold') return <p className={`${style.txt_caption_bold} ${moreClassStyled}`}>{children}</p>
+  if (styleToken == 'detail') return <p className={`${style.txt_detail} ${moreClassStyled}`}>{children}</p>
   
 }
 
 TextPortfolio.propTypes = {
   children: PropTypes.node,
-  styleToken: PropTypes.string
+  styleToken: PropTypes.string,
+  moreClassStyled: PropTypes.string
 };
