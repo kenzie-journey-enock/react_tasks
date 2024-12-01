1a38982c974bce445776abf8f1f0bff76288407d
bernardoenock <bernardoenockgm@gmail.com>
2024-12-01 15:21:16 -0300

    feat: BannerProjects

---
+++ src/studies/Projects/Portifolio/components/atoms/BannerProjectsPortfolio.jsx
@@ -0,0 +1,9 @@
+import style from "./style.module.css"
+
+export default function BannerProjectsPortfolio() {
+  return (
+    <div className={style.banner_projects}>
+      <img src="src/assets/frame_projects_kenzie.png" alt="A Banner of my projects" />
+    </div>
+  )
+}
