1a93098fb0cf684bcdd903aeda389dfd94668e51
bernardoenock <bernardoenockgm@gmail.com>
2024-12-01 15:23:58 -0300

    fix: Organize

--- src/studies/Projects/Portifolio/components/molecules/AdvertMePortfolio.jsx
+++ src/studies/Projects/Portifolio/components/molecules/AdvertMePortfolio.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types'
 import parse from "html-react-parser";
 
-import TextPortfolio from '../atoms/TextPortfolio'
 import style from './style.module.css'
 
+import AvatarPortfolio from '../atoms/AvatarPortfolio';
 import TitlePortfolio from '../atoms/TitlePortfolio'
+import TextPortfolio from '../atoms/TextPortfolio'
 import BtnPortifolio from '../atoms/BtnPortfolio';
 
-export default function AdvertMePortfolio({data}){
+export default function AdvertMePortfolio({ data }) {
   return (
     <div className={style.advertme_container}>
-      <TitlePortfolio styleToken='title1'>{parse(data.advert_me.call)}</TitlePortfolio>
+      <AvatarPortfolio name={data.name} src='https://picsum.photos/536/354' />
+      <TitlePortfolio styleToken='title1' styleMKT={style.underline_effect}>{parse(data.advert_me.call)}</TitlePortfolio>
       <TextPortfolio styleToken='headline'>{data.advert_me.action}</TextPortfolio>
       <div className={style.btn_projects}>
         <BtnPortifolio styleToken='primary'>Projects</BtnPortifolio>
--- src/studies/Projects/Portifolio/components/organisms/MainPortfolio.jsx
+++ src/studies/Projects/Portifolio/components/organisms/MainPortfolio.jsx
@@ -1,16 +1,12 @@
 import style from './style.module.css'
-
-import AvatarPortfolio from '../atoms/AvatarPortfolio'
-import AdvertMePortfolio from '../molecules/AdvertMePortfolio'
 import PropTypes from 'prop-types'
 
-export default function MainPortfolio({data}){
+import FirstSectionPortfolio from '../molecules/FirstSectionPortfolio'
+
+export default function MainPortfolio({ data }) {
   return (
     <main className={style.main_portfolio}>
-      <section className={style.first_s}>
-        <AvatarPortfolio name={data.name} src='https://picsum.photos/536/354'/>
-        <AdvertMePortfolio data={data} />
-      </section>
+      <FirstSectionPortfolio data={data} />
     </main>
   )
 }
