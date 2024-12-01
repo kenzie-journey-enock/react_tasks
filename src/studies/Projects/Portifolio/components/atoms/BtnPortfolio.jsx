1cf9eb13533879f4f53ab2b1c56ded9da41f43cb
bernardoenock <bernardoenockgm@gmail.com>
2024-12-01 18:33:22 -0300

    feat: Adds more dynamism to btn

--- src/studies/Projects/Portifolio/components/atoms/BtnPortfolio.jsx
+++ src/studies/Projects/Portifolio/components/atoms/BtnPortfolio.jsx
@@ -2,9 +2,19 @@ import style from "./style.module.css"
 
 import PropTypes from 'prop-types'
 
-export default function BtnPortifolio({children, styleToken}){
+export default function BtnPortifolio({ children, styleToken, colorBg, hoverColorBg }) {
   return (
-    <button className={setStyleClass(styleToken, style)}>
+    <button className={setStyleClass(styleToken, style)} style={{
+      backgroundColor: colorBg,
+      transition: 'background-color 0.3s, color 0.3s, border 0.6s'
+    }}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.backgroundColor = hoverColorBg;
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.backgroundColor = colorBg;
+      }}
+    >
       {children ? children : 'Vazio'}
     </button>
   )
@@ -12,11 +22,13 @@ export default function BtnPortifolio({children, styleToken}){
 
 BtnPortifolio.propTypes = {
   children: PropTypes.node,
-  styleToken: PropTypes.string
+  styleToken: PropTypes.string,
+  colorBg: PropTypes.string,
+  hoverColorBg: PropTypes.string
 };
 
 
-function setStyleClass(styleToken, style){
+function setStyleClass(styleToken, style) {
   if (styleToken == 'primary') return style.btn_primary
 
   if (styleToken == 'secoundary') return style.btn_secoundary
