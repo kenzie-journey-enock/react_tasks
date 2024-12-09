033311a8f894e6eab489e2eb2f2e77ce3e09a11e
bernardoenock <bernardoenockgm@gmail.com>
2024-12-09 20:51:42 -0300

    feat: Create ThirdSectionPortfolio

--- src/studies/Projects/Portifolio/components/molecules/sections/ThirdSectionPortfolio.jsx
+++ src/studies/Projects/Portifolio/components/molecules/sections/ThirdSectionPortfolio.jsx
@@ -1,10 +1,15 @@
 import style from './style.module.css'
 import PropTypes from 'prop-types'
 
+import mePixel from '../../../assets/me_pixel.png'
+import MePixelPortfolio from '../../atoms/MePixelPortfolio'
+import ContactMePortfolio from '../boxes/ContactMePortfolio'
+
 export default function ThirdSectionPortfolio({ data }) {
   return (
     <section id={data.sections[2].ref} className={style.third_section}>
-      Contacts {data.name}
+      <ContactMePortfolio data={data} />
+      <MePixelPortfolio imgURL={mePixel} alt="Me in AI pixel art" />
     </section>
   )
 }
