64c42b6a5da7cf1e3bdbe1e4bed577301ec18540
bernardoenock <bernardoenockgm@gmail.com>
2024-12-23 16:26:16 -0300

    feat: Add content to Footer

--- src/studies/Projects/Portifolio/components/organisms/FooterPortfolio.jsx
+++ src/studies/Projects/Portifolio/components/organisms/FooterPortfolio.jsx
@@ -1,10 +1,12 @@
 import style from './style.module.css'
 import PropTypes from 'prop-types'
 
+import AvatarThanksPortfolio from '../atoms/AvatarThanksPortfolio'
+
 export default function FooterPortfolio({ data }) {
   return (
     <footer className={style.footer_portfolio}>
-      RODAPÉ
+      <AvatarThanksPortfolio name={data.name} src='https://picsum.photos/536/354' />
     </footer>
   )
 }
