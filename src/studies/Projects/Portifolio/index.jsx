afa2f11bce141da2c50cbf2d662214e9554971eb
bernardoenock <bernardoenockgm@gmail.com>
2024-12-23 16:22:39 -0300

    feat: Add footer

--- src/studies/Projects/Portifolio/index.jsx
+++ src/studies/Projects/Portifolio/index.jsx
@@ -6,6 +6,7 @@ import data from './data_profile.json'
 import projectImgExample from './assets/projects/objective_task.jpg'
 
 import {
+  FooterPortfolio,
   HeaderPortifolio,
   MainPortfolio
 } from "./components/organisms";
@@ -30,22 +31,7 @@ export default function Portifolio() {
     <>
       <HeaderPortifolio data={dataProfile} />
       <MainPortfolio data={dataProfile} />
+      <FooterPortfolio data={dataProfile} />
     </>
   )
 }
-
-
-
-//  {/* <BtnPortifolio styleToken='primary' >TESTANDO BTN</BtnPortifolio>
-//       <BtnPortifolio styleToken='secoundary' >TESTANDO BTN 2</BtnPortifolio>
-
-//       <TextPortfolio styleToken='title1' >Title1</TextPortfolio>
-//       <TextPortfolio styleToken='title2' >Title2</TextPortfolio>
-//       <TextPortfolio styleToken='title3' >Title3</TextPortfolio>
-//       <TextPortfolio styleToken='title4' >Title4</TextPortfolio>
-
-//       <TextPortfolio styleToken='headline' >headline</TextPortfolio>
-//       <TextPortfolio styleToken='body' >body</TextPortfolio>
-//       <TextPortfolio styleToken='caption' >caption</TextPortfolio>
-//       <TextPortfolio styleToken='caption_bold' >caption_bold</TextPortfolio>
-//       <TextPortfolio styleToken='detail' >detail</TextPortfolio> */}
