0c6894d4c5ff266993374aad2f8bb5452c96595e
bernardoenock <bernardoenockgm@gmail.com>
2025-02-15 17:32:18 -0300

    feat: Add list items

--- src/studies/Projects/Finok/components/molecules/FinSummaryFinok.jsx
+++ src/studies/Projects/Finok/components/molecules/FinSummaryFinok.jsx
@@ -1,16 +1,28 @@
+import PropTypes from 'prop-types';
+import { FinSummary, FinContainerItems } from "./StyleMolecules";
+
 import TextFinok from "../atoms/TextFinok";
 import FinItemFinok from "./FinItemFinok";
 
-import { FinSummary } from "./StyleMolecules";
-
-export default function FinSummaryFinok() {
+export default function FinSummaryFinok({ items, setItem }) {
   return (
     <FinSummary>
       <TextFinok styleToken='title3'>Resumo Financeiro</TextFinok>
 
-      <TextFinok styleToken='title2'>Você ainda não possui nenhum lançamento</TextFinok>
-
-      <FinItemFinok />
+      {items && items.length > 0 ? (
+        <FinContainerItems >
+          {items.map((item) =>
+            <FinItemFinok key={item.id} item={item} setItem={setItem} />
+          )}
+        </FinContainerItems>
+      ) : (
+        <TextFinok styleToken='title2'>Você ainda não possui nenhum lançamento</TextFinok>
+      )}
     </FinSummary>
-  )
+  );
 }
+
+FinSummaryFinok.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+  setItems: PropTypes.func.isRequired,
+};
