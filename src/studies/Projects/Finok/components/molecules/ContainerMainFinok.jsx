c1ed3ca2df5acfbd904451dacc26c2435a37ea6a
bernardoenock <bernardoenockgm@gmail.com>
2025-02-15 17:31:18 -0300

    feat: Add dinamic data

--- src/studies/Projects/Finok/components/molecules/ContainerMainFinok.jsx
+++ src/studies/Projects/Finok/components/molecules/ContainerMainFinok.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import { ContainerMain } from './StyleMolecules'
 
 import FinFormFinok from './FinFormFinok'
 import FinSummaryFinok from './FinSummaryFinok'
 
+import data_mock from "../../data_finok.json"
+
 export default function ContainerMainFinok() {
+  const [resume, setResume] = useState(data_mock.resume)
+
   return (
     <ContainerMain>
-      <FinFormFinok />
-      <FinSummaryFinok />
+      <FinFormFinok setItem={setResume} />
+      <FinSummaryFinok items={resume} setItem={setResume} />
     </ContainerMain>
   )
 }
