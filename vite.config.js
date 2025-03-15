8d0ce313ba5057a21368478b83877fde1d631169
bernardoenock <bernardoenockgm@gmail.com>
2025-03-15 19:09:00 -0300

    fix: warning sass 2.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sass from 'sass-embedded';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass
+      }
+    }
+  },
   server: {
     port: 3000,
   },
   preview: {
     port: 5000,
   },
-  base: '/react_tasks/',
-})
+  base: '/react_tasks/'
+});
