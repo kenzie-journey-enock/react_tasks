967eb26589452d877fd695db3f7ac93919b2fda9
Bernardo Enock <bernardoenockgm@gmail.com>
2025-05-09 13:58:19 -0300

    feat: Create BtnAddNockBurguer

---
+++ src/studies/Projects/NockBurguer/components/atoms/BtnAddNockBurguer.jsx
@@ -0,0 +1,8 @@
+import { BtnAddNockBurguerStyle } from "./StyleAtoms";
+
+
+export default function BtnAddNockBurguer() {
+  return (
+    <BtnAddNockBurguerStyle>Adicionar</BtnAddNockBurguerStyle>
+  )
+}
