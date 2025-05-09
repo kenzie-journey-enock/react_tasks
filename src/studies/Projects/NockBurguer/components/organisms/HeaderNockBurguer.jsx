71426bc70b2706cbac55c1e7b185df128a110099
Bernardo Enock <bernardoenockgm@gmail.com>
2025-05-09 11:23:41 -0300

    feat: Add Container in HeaderNockBurguer

--- src/studies/Projects/NockBurguer/components/organisms/HeaderNockBurguer.jsx
+++ src/studies/Projects/NockBurguer/components/organisms/HeaderNockBurguer.jsx
@@ -1,11 +1,12 @@
 
+import ContainerHeaderNockBurguer from "../molecules/ContainerHeaderNockBurguer"
 import { HeaderNockBurguerStyle } from "./StyleOrganisms"
 
 export default function HeaderNockBurguer() {
 
   return (
     <HeaderNockBurguerStyle>
-      <h1>Header Nock Burguer</h1>
+      <ContainerHeaderNockBurguer />
     </HeaderNockBurguerStyle>
   )
 }
