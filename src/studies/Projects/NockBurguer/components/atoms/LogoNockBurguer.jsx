6aafd065c8bfeb814477a214a5fef02412d1cfe9
Bernardo Enock <bernardoenockgm@gmail.com>
2025-05-09 11:44:18 -0300

    feat: Create LogoNockBurguer

---
+++ src/studies/Projects/NockBurguer/components/atoms/LogoNockBurguer.jsx
@@ -0,0 +1,10 @@
+import { LogoNockBurguerStyle } from "./StyleAtoms";
+
+export default function LogoNockBurguer() {
+  return (
+    <LogoNockBurguerStyle>
+      <h1>Nock</h1>
+      <h2>Burguer</h2>
+    </LogoNockBurguerStyle>
+  )
+}
